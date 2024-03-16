3d241fc67687a1694d15f50df56784c273e8fc96
Aseeeem-kc <ashimkc7297@gmail.com>
2024-03-16 22:31:27 +0545

    oop for pythonoop tut

--- pythonOOP.py
+++ pythonOOP.py
@@ -1,94 +1,37 @@
-class Employee:
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    raise_amount = 1.04
-
-    def __init__ (self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
-
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-    
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
-    
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amount = amount
-
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
-
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    def sayHi(self):
+        print("Hello!" + self.name) 
 
     def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
+        return f"{self.name}({self.age})"
+    
     
 
-    def __add__(self, other):
-        return self.pay + other.pay
-        
+class Student(Person):
+    def __init__(self, name,age, major):
+        super().__init__(name, age)
+        self.major = major
 
-class Developer(Employee):
-    raise_amount = 1.10
+    def marks(self):
+        print("The student got 30 out of 50!")
 
-    def __init__ (self, first, last, pay, lang):
-        super().__init__(first, last, pay)
-        self.lang = lang
-    
+class Driver(Person):
+    def __init__(self, name, age, vehicle):
+        super().__init__(name, age)
+        self.vehicle = vehicle
 
-class Manager(Employee):
-     
-    def __init__ (self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
-
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)   
-
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
+    def distance(self):
+        print("The driver has drived over 100,000 miles")
 
 
-    
-dev_1 = Employee('Corey', 'Schafer', 50000)
-dev_2 = Employee('Test', 'Employee', 60000)
-dev_3 = Developer('kc', 'ashim', 69000, 'bhailang')
 
-print(dev_1 + dev_2)
-# print(help(Developer))
+s1 = Student("John",30, "Science")
+d1 = Driver("rahul",43, "Truck")
 
-# mgr_1 = Manager("sue", "smith", 90000, [dev_1,dev_2])
+s1.marks()
+d1.distance()
 
-# print(mgr_1.email)
-# mgr_1.add_emp(dev_3)
-# # mgr_1.remove_emp(dev_2)
-
-# mgr_1.print_emps()
-
-# # print(dev_1.lang)
-# # dev_1.apply_raise()
-# # print(dev_1.pay)
-
-
-# print(issubclass(Manager, Developer))
-
-# print(dev_1)
-
-# print(repr(dev_1))
-# print(str(dev_1))
